111e12bc574d99cf321a16148bff9c0a54bbc96d
Beomi <latheledusjp@gmail.com>
2016-12-16 00:47:22 +0900

    remove unused views / add selenium for TDD

--- autonomy/api_urls.py
+++ autonomy/api_urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from .api_views import student_verify
+#from .api_views import student_verify
 
 
 urlpatterns = [
-    url(r'^auth/$', student_verify, name='auth'),
+#    url(r'^auth/$', student_verify, name='auth'),
 
 ]
